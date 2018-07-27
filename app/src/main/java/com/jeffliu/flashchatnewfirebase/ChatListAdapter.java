7139ab2acfb7f7b2397949c5d44fbc92deb45df7
Jeff Liu <boyuanliu@live.com>
2018-07-27 15:44:06 -0400

    implemented viewholder pattern for smooth scrolling

--- app/src/main/java/com/jeffliu/flashchatnewfirebase/ChatListAdapter.java
+++ app/src/main/java/com/jeffliu/flashchatnewfirebase/ChatListAdapter.java
@@ -1,7 +1,9 @@
 package com.jeffliu.flashchatnewfirebase;
 
 import android.app.Activity;
+import android.content.Context;
 import android.provider.ContactsContract;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -40,7 +42,7 @@ public class ChatListAdapter extends BaseAdapter{
     }
 
     @Override
-    public Object getItem(int position) {
+    public InstantMessage getItem(int position) {
         return null;
     }
 
@@ -51,6 +53,30 @@ public class ChatListAdapter extends BaseAdapter{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null){
+            LayoutInflater inflater = (LayoutInflater) mActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.chat_msg_row, parent, false);
+
+            final ViewHolder holder = new ViewHolder();
+            holder.authorName = (TextView) convertView.findViewById(R.id.author);
+            holder.body = (TextView) convertView.findViewById(R.id.message);
+            holder.params = (LinearLayout.LayoutParams) holder.authorName.getLayoutParams();
+            convertView.setTag(holder);
+        }
+
+        final InstantMessage message = getItem(position);
+        final ViewHolder holder = (ViewHolder) convertView.getTag();
+
+        String author = message.getAuthor();
+        holder.authorName.setText(author);
+
+        String msg = message.getAuthor();
+        holder.body.setText(msg);
+
+
+
+
+
         return null;
     }
 }
